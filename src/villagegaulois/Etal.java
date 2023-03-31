65c6149d0ddcddcecf55345354644d6333ae8efd
DNTtlse3 <thaoumani.dini-ali@univ-tlse3.fr>
2023-03-31 12:16:19 +0200

    DINI ALI ver 1.12

--- src/villagegaulois/Etal.java
+++ src/villagegaulois/Etal.java
@@ -1,9 +1,20 @@
 package villagegaulois;
 
-public class Etal {
+import personnages.Gaulois;
+import produit.IProduit;
 
-	public Etal() {
-		// TODO Auto-generated constructor stub
+public class Etal <P extends IProduit> {
+	
+	private Gaulois vendeur;
+	private P[] tabProduit;
+	private int quantiteProduit;
+	private int prixProduit; 
+	
+	
+	public void installerVendeur(Gaulois vendeur, P[] produit, int prix) {
+		this.vendeur = vendeur;
+		this.tabProduit = produit;
+		this.prixProduit = prix;
+		
 	}
-
 }
